790ea5c4e2dfc9b3cac9a15ac2a0b378d58bad19
Brady <duncan.br@northeastern.edu>
2022-09-28 15:25:49 -0400

    partial assignment

---
+++ main.js
@@ -0,0 +1,15 @@
+// JS Code for HW3
+// Brady Duncan and Evan Suslovich
+// Last Modified: 09/28/2022
+
+// create a function for when the first button is clicked
+//can we make this an abstract function?
+function marksButton1Clicked() {
+    let newText = 'Marks: Area, Channels: Color (Hue)';
+
+    document.getElementById('marks1').innerHTML(newText);
+}
+
+// add event handler for the marks and channels button
+//if we have an abstract funciton, this can get getElementByClass and have it be all buttons
+document.getElementById('marks_button1').addEventListener('click', marksButton1Clicked)
