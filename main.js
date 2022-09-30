08bc7c6b5f97717ee37db20c9c9492647d47bf37
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-09-30 09:13:06 -0400

    visibility

--- main.js
+++ main.js
@@ -6,32 +6,39 @@
 //can we make this an abstract function?
 function marksButtonClicked1() {
     let newText = 'Marks: Area, Channels: Color (Hue)';
-
+    
     document.getElementById('marks_button1').classList.toggle("collapse");
+    document.getElementById("marks1").classList.add("marks1Visible");
+    
+    
 }
 
 
 
 function colorMapsButtonClicked1() {
-
+    
     document.getElementById('colormap_button1').classList.toggle("collapse");
+    document.getElementById("colormaps1").classList.add("colorMaps1Visible");
 
+    
 }
 
 
 function marksButtonClicked2() {
-
+    
     document.getElementById('marks_button2').classList.toggle("collapse");
+    document.getElementById("marks2").classList.add("marks2Visible");
 
-
+    
 }
 
 
 function colorMapsButtonClicked2() {
-
+    
     document.getElementById('colormap_button2').classList.toggle("collapse");
+    document.getElementById("colormaps2").classList.add("colorMaps2Visible");
 
-
+    
 }
 
 
